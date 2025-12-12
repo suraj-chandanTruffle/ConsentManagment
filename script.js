013f516c78908d378dc563e5cffacc2099713f94
suraj-chandanTruffle <suraj.chandan@trufflecorp.com>
2025-12-12 13:30:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const token = "QVZHMDAwMDAwVWhzTDYzeTNadnlIdkdCSnR0emh2LzZnaXlrYUF3Q29YdVFTM050YjdkZkNyRzE5S2xYOWF3WHZIbHE3T1RWK0x6NnJianovY3JVY3hxTERiSjQyTUU2QlJJTUV4ZVZKeU16KzF0UVZ1SmpmU0svNWZzZUdmRTZpczd5VURCZVBiZ21WSXRnSHNtUStwZFpFUHNZWkhDZ1hQQT09";
+const newUrl = window.location.origin + window.location.pathname + "?pctoken=" + encodeURIComponent(token);
+window.history.replaceState({}, "", newUrl);
+
 function queryNestedShadow(root, selector) {
   let results = [];
     console.log('queryNestedShadow');
