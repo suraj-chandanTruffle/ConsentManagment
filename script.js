4283d4742d17120bc3c925bb5dcd2e34591ba480
suraj-chandanTruffle <suraj.chandan@trufflecorp.com>
2025-12-12 16:30:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const token = "QVZHMDAwMDAwVWhzTDYzeTNadnlIdkdCSnR0emh2LzZnaXlrYUF3Q29YdVFTM050YjdkZkNyRzE5S2xYOWF3WHZIbHE3T1RWK0x6NnJianovY3JVY3hxTERiSjQyTUU2QlJJTUV4ZVZKeU16KzF0UVZ1SmpmU0svNWZzZUdmRTZpczd5VURCZVBiZ21WSXRnSHNtUStwZFpFUHNZWkhDZ1hQQT09";
-const newUrl = window.location.origin + window.location.pathname + "?pctoken=" + encodeURIComponent(token);
-window.history.replaceState({}, "", newUrl);
+const params = new URLSearchParams(window.location.search);
+if (!params.has('pctoken')) {
+  const url = new URL(window.location.href);
+  url.searchParams.set("pctoken", token);
+  window.location.href = url.href;
+}
