3430292d103fa4bb200a1456e610257c1cbd5b40
Supehia95 <sandeep.kumar@trufflecorp.com>
2025-12-12 17:38:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const token = "QVZHMDAwMDAwVWhzTDYzeTNadnlIdkdCSnR0emh2LzZnaXlrYUF3Q29YdVFTM050YjdkZkNyRzE5S2xYOWF3WHZIbHE3T1RWK0x6NnJianovY3JVY3hxTERiSjQyTUU2QlJJTUV4ZVZKeU16KzF0UVZ1SmpmU0svNWZzZUdmRTZpczd5VURCZVBiZ21WSXRnSHNtUStwZFpFUHNZWkhDZ1hQQT09";
-const params = new URLSearchParams(window.location.search);
-if (!params.has('pctoken')) {
- const url = new URL(window.location.href);
- url.searchParams.set("pctoken", token);
- window.location.href = url.href;
-}
+
